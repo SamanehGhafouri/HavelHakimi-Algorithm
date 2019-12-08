aec870adc1ac7a6bdd765af8804434745139d4d7
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2019-12-07 19:51:50 -0500

    preliminary code setup before adding non-graphic logic to hha

--- havel_hakimi_algorithm.py
+++ havel_hakimi_algorithm.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Tuple
 
 
+# find the max value in the list for giving a list of numbers return index and number
 def find_max_index_in_list(sequence: List[int]) -> (int, int):
 
 	max_index = 0
@@ -15,7 +16,7 @@ def find_max_index_in_list(sequence: List[int]) -> (int, int):
 
 
 def find_n_max_index_in_list(n: int, sequence: List[int]) -> List[Tuple[int, int]]:
-
+	# we want to modify the sequence so we make a copy
 	c_sequence = sequence.copy()
 
 	result: List[(int, int)] = []
@@ -24,37 +25,47 @@ def find_n_max_index_in_list(n: int, sequence: List[int]) -> List[Tuple[int, int
 		index, value = find_max_index_in_list(c_sequence)
 		result.append((index, value))
 
+		# index + 1 will include the index itself
 		for pre_index in range(0, index + 1):
+			# we already found it so it is equal to 0
 			c_sequence[pre_index] = 0
 
 	return result
 
 
 # An arrow -> indicates a return type will follow
-def hha(vertices: List[str], sequence: List[int]) -> Dict[str, List[str]]:
+def hha(vertices: List[str], sequence: List[int]) -> Tuple[Dict[str, List[str]], bool]:
 
-	result: Dict[str, List[str]] = {}
+	is_graphic = True
+	adjacency_list: Dict[str, List[str]] = {}
+	# initialize the adjacency list with empty list of neighbours
 	for vertex_key in vertices:
-		result[vertex_key] = []
+		adjacency_list[vertex_key] = []
 
 	print("It's Havel Time!!!")
 
 	while True:
+		# finding the max number
 		m_index, m_value = find_max_index_in_list(sequence)
+		# if the largest value we found is 0 we stop
 		if m_value == 0:
 			# stopping condition
 			break
+		# update the sequence the max is now 0
 		sequence[m_index] = 0
 		list_of_max = find_n_max_index_in_list(m_value, sequence)
 		for pair in list_of_max:
+			# pair[index, value] only care about the index
 			target_index = pair[0]
+			# decrement the max numbers
 			sequence[target_index] -= 1
 
 			# print(f"Vertex Pairs({vertices[m_index]}, {vertices[target_index]})")
-
-			neighbours = result[vertices[m_index]]
+			neighbours = adjacency_list[vertices[m_index]]
+			# add a neighbour to the vertex at index m
 			neighbours.append(vertices[target_index])
-			neighbours = result[vertices[target_index]]
+
+			neighbours = adjacency_list[vertices[target_index]]
 			neighbours.append(vertices[m_index])
 
-	return result
+	return adjacency_list, is_graphic
--- test_hakimi.py
+++ test_hakimi.py
@@ -4,10 +4,12 @@ from havel_hakimi_algorithm import find_max_index_in_list
 from havel_hakimi_algorithm import find_n_max_index_in_list
 
 
+# test the code
 class TestBasicHakimi(TestCase):
 
-	def test_in_class_example_one(self):
+	def test_graphic_in_class_example_one(self):
 
+		# adjacency list that we expect
 		expected = {
 			'A': ['B', 'C', 'D', 'E'],
 			'B': ['A', 'C', 'F', 'G'],
@@ -19,35 +21,80 @@ class TestBasicHakimi(TestCase):
 			'H': ['G']
 		}
 		sequence = [4, 4, 3, 2, 2, 2, 2, 1]
+
+		# returning the keys and sort them
 		vertices = sorted(expected.keys())
 
-		actual = hha(vertices, sequence)
+		actual, _ = hha(vertices, sequence)
+
+		# check if the expected is equal to actual
 		self.assertEqual(expected, actual)
 
+	def test_graphic_in_class_example_one(self):
+
+		# adjacency list that we expect
+		expected = True
+		sequence = [4, 4, 3, 2, 2, 2, 2, 1]
+
+		# returning the keys and sort them
+		vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+		_, actual = hha(vertices, sequence)
+
+		# check if the expected is equal to actual
+		self.assertEqual(expected, actual)
+
+	def test_non_graphic_sequence_one(self):
+
+		expected = False
+		sequence = [4, 3, 3, 2, 2, 2, 2, 1]
+
+		vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+		_, actual = hha(vertices, sequence)
+
+		self.assertEqual(expected, actual)
+
+	def test_non_graphic_sequence_two(self):
+
+		expected = False
+		sequence = [4, 4, 3, 3, 2, 2, 2, 2, 1]
+
+		vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+
+		_, actual = hha(vertices, sequence)
+
+		self.assertEqual(expected, actual)
+
+	# test to find the max value function
 	def test_begin_max_value_from_sequence(self):
 		sequence = [4, 1, 2, 0]
 		expected = (0, 4)
 		actual = find_max_index_in_list(sequence)
 		self.assertEqual(expected, actual)
 
+	# test if the max value in the middle for the same function finding the max
 	def test_middle_max_value_from_sequence(self):
 		sequence = [4, 1, 8, 2, 0]
 		expected = (2, 8)
 		actual = find_max_index_in_list(sequence)
 		self.assertEqual(expected, actual)
 
+	# test if the max is at the end
 	def test_end_max_value_from_sequence(self):
 		sequence = [4, 1, 8, 2, 10]
 		expected = (4, 10)
 		actual = find_max_index_in_list(sequence)
 		self.assertEqual(expected, actual)
 
+	# test for finding the nth max in the list(3 biggest)
 	def test_3_max_values_from_sequence(self):
 		sequence = [0, 3, 2, 1, 1, 2, 2, 1]
 		expected = [(1, 3), (2, 2), (5, 2)]
 		actual = find_n_max_index_in_list(3, sequence)
 		self.assertEqual(expected, actual)
 
+	# test for finding the nth max( 4 biggest)
 	def test_4_max_values_from_sequence(self):
 		sequence = [0, 3, 2, 1, 1, 2, 2, 1]
 		expected = [(1, 3), (2, 2), (5, 2), (6, 2)]
